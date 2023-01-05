95c3a484bb6e103057e6983eb7859121385a7eb1
Philip Mattsson <philipm606@gmail.com>
2023-01-05 14:09:54 +0100

    Added Repository, Service & Controller

---
+++ student-management-system/src/main/java/net/javaguides/sms/controller/StudentController.java
@@ -0,0 +1,23 @@
+package net.javaguides.sms.controller;
+
+import net.javaguides.sms.service.StudentService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class StudentController {
+
+    private StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        super();
+        this.studentService = studentService;
+    }
+
+    @GetMapping("/students")
+    public String listStudents(Model model){
+        model.addAttribute("students", studentService.getAllStudents());
+        return "students";
+    }
+}
